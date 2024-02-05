a51a6bbbb40ef1a8bedd6ad9a4432ab7f2990925
Fabio Battaglia <tabaglio@posteo.net>
2024-02-05 21:05:00 +0100

    Add command codes

---
+++ src/command_codes.py
@@ -0,0 +1,15 @@
+from enum import Enum
+
+class Command_Codes(Enum):
+    SYNC = 0
+    ECHO = 1
+    WRITE_MEM = 2
+    READ_MEM = 3
+    GET_INFO = 4
+    EXEC_DEBUG = 5
+    EXEC_MEM = 6
+    WRITE_FLASH = 7
+    READ_FLASH = 8
+    CLEAR_FLASH = 9
+    CHECK_FLASH = 10
+    EXEC_FLASH = 11
